18be9c42cf7d99f274d7b6588703c4137a407d04
IKSAE <iksae123@gmail.com>
2024-10-17 17:01:49 +0900

    Add files via upload

---
+++ Flutter/Flutter02/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final logger = Logger();
+
+  // const 키워드를 제거
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          key: const Key('appbar'), // AppBar에 key 추가
+          title: const Text('플러터 앱 만들기'),
+          backgroundColor: Colors.blue,
+          leading: const Icon(Icons.menu),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('Text'),
+              ElevatedButton(
+                onPressed: () {
+                  logger.i('버튼이 눌렸습니다'); // logger 사용
+                },
+                child: const Text('Button'),
+              ),
+              Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  Container(width: 300, height: 300, color: Colors.red),
+                  Container(width: 240, height: 240, color: Colors.orange),
+                  Container(width: 180, height: 180, color: Colors.yellow),
+                  Container(width: 120, height: 120, color: Colors.green),
+                  Container(width: 60, height: 60, color: Colors.blue),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
